baf6649d43219d0eea20066e655965933eec6f43
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-02-10 23:33:30 -0500

    Added attributes/props to the JSX code, and accessed the props in the Person component.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,10 +8,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, React app here.</h1>
         <p>This works fine too.</p>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <Person name = 'PT' age = '28'/>
+        <Person name = 'UP' age = '28'> Her hobbies: Working</Person>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Let\'s see if this works.'));
