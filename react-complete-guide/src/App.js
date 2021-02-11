382bdad44fb3e109c49411fe6e9d1a119b40262c
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-02-11 18:50:54 -0500

    Adding inline style in the App.js file.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -33,11 +33,21 @@ import Person from './Person/Person';
   } 
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+    
     return (
       <div className="App">
         <h1>Hi, React app here.</h1>
         <p>This works fine too.</p>
-        <button onClick={() => this.switchNameHandler('Swaroop!!')}>Switch Name</button>
+        <button 
+        onClick={() => this.switchNameHandler('Swaroop!!')}
+        style={style}>Switch Name</button>
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age} 
