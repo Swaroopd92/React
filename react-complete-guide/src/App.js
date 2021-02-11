5c331439d3fa8f03715ce6e02df9ba2b0d6328d7
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-02-11 02:51:18 -0500

    Using react hooks and fuctional programming for State manipulation instead of Class programming by extending Components.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,39 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+ const app = props => {
+
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'PT', age: 28 },
       { name: 'UP', age: 28 }
     ],
-    otherState: 'Some value'
-  }
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState('some value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
     // console.log('Was clicked.');
     // DON"T DO THIS this.state.persons[0].name = 'Swaroop';
-    this.setState({
+    setPersonsState({
       persons: [
         { name: 'Swaroop', age: 28 },
         { name: 'Priya', age: 28 }
       ]
-    })
+    });
   }
 
-  render() {
     return (
       <div className="App">
         <h1>Hi, React app here.</h1>
         <p>This works fine too.</p>
-        <button onClick={this.switchNameHandler}>Switch_Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> Her hobbies: Working 24/7</Person>
+        <button onClick={switchNameHandler}>Switch_Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> Her hobbies: Working 24/7</Person>
       </div>
     );
     // return React.createElement('div', {classNam  e: 'App'}, React.createElement('h1', null, 'Let\'s see if this works.'));
-  }
 }
 
-export default App;
+export default app;
