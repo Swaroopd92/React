75b9120f74cfca620311b354e5787e90840270e7
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-02-10 23:12:36 -0500

    Adding the smae component multiple times. Noting that the content of the compnent is still static.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi, React app here.</h1>
         <p>This works fine too.</p>
-        <Person/>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Let\'s see if this works.'));
