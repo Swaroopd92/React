f10221df3e62f7eddb5587740ab460c7ec6c5c77
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-02-10 23:51:36 -0500

    Added state property from the 'Component' class to remove the hard coded name and age values inside the component.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'PT', age: 28 },
+      { name: 'UP', age: 28 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, React app here.</h1>
         <p>This works fine too.</p>
-        <Person name = 'PT' age = '28'/>
-        <Person name = 'UP' age = '28'> Her hobbies: Working 24/7</Person>
+        <button>Switch_Name</button>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}> Her hobbies: Working 24/7</Person>
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Let\'s see if this works.'));
+    // return React.createElement('div', {classNam  e: 'App'}, React.createElement('h1', null, 'Let\'s see if this works.'));
   }
 }
 
